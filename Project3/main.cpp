7afbf0c4fbc2d36ff1a2782a6c760bef64bc18b8
Omer <omerfaunal@outlook.com>
2022-12-27 00:14:50 +0300

    Configured input output format

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,3 +1,10 @@
+/*
+@Author: Omer Faruk Unal
+
+You can run the program with the following command: 
+make run input={input_path}
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,15 +17,16 @@
 using namespace std;
 
 int num_customers;
+//int customer_no;
 
 ofstream fileOut;
 pthread_mutex_t writeLock;
 pthread_mutex_t machineLock[11];
 pthread_mutex_t machineLock2[11];
+//pthread_mutex_t customerNoLock;
 
 vector<string> transactions(11);
 
-//Kevin, Bob, Stuart, Otto, Dave
 int kevin = 0;
 int bob = 0;
 int stuart = 0;
@@ -33,7 +41,7 @@ void *customer(void *param);
 int main(int argc,  char **argv){
 
     string fileName = argv[1]; 
-    vector<string> lines(num_customers);
+    vector<string> lines;
 
     ifstream file(fileName);
     if (file.is_open()) {
@@ -48,7 +56,7 @@ int main(int argc,  char **argv){
         file.close();
     }
 
-    string outFileName = "output.txt";
+    string outFileName = fileName.substr(0, fileName.size() - 4) + "_log.txt";
     fileOut.open(outFileName);
     pthread_t machines[11];
     int nums[11];
@@ -58,6 +66,7 @@ int main(int argc,  char **argv){
     for (int i = 1; i < 11; i++) {
         pthread_mutex_init(&machineLock[i], NULL);
         pthread_mutex_init(&machineLock2[i], NULL);
+        //pthread_mutex_init(&customerNoLock, NULL);
         pthread_mutex_lock(&machineLock[i]);
     }
 
@@ -69,21 +78,20 @@ int main(int argc,  char **argv){
         
     for (int i = 0; i < num_customers; i++) 
         pthread_create(&customers[i], 0, customer, &lines[i]);
+
+    for (int i = 0; i < num_customers; i++)
+        pthread_join(customers[i], NULL);
     
     for (int i = 1; i < 11; i++) 
         pthread_join(machines[i], NULL);
     
-    for (int i = 0; i < num_customers; i++)
-        pthread_join(customers[i], NULL);
 
-
-    fileOut << "All prepayments are completed." << endl;
-    fileOut << "Kevin: " << kevin << "TL" << endl;
-    fileOut << "Bob: " << bob << "TL" << endl;
-    fileOut << "Stuart: " << stuart << "TL" << endl;
-    fileOut << "Otto: " << otto << "TL" << endl;
-    fileOut << "Dave: " << dave << "TL";
-
+    fileOut << "[Main]: All prepayments are completed." << endl;
+    fileOut << "[Main]: Kevin: " << kevin << "TL" << endl;
+    fileOut << "[Main]: Bob: " << bob << "TL" << endl;
+    fileOut << "[Main]: Stuart: " << stuart << "TL" << endl;
+    fileOut << "[Main]: Otto: " << otto << "TL" << endl;
+    fileOut << "[Main]: Dave: " << dave << "TL";
     fileOut.close();
     
     return 0;
@@ -105,7 +113,12 @@ void* customer(void *param) {
     sleep = stoi(v[0]);
     machineNo = stoi(v[1]);
     usleep(sleep * 1000);
-    
+
+    // pthread_mutex_lock(&customerNoLock);
+    // line.append(",Customer" + to_string(customer_no + 1));
+    // customer_no++;
+    // pthread_mutex_unlock(&customerNoLock);
+
     pthread_mutex_lock(&machineLock2[machineNo]);
     transactions[machineNo] = line;
     pthread_mutex_unlock(&machineLock[machineNo]);
@@ -158,7 +171,7 @@ void* machine(void *param) {
         else if (name == "Dave")
             dave += amount; ;
 
-        string log = v[4] + "," + v[3] + "TL" + "," + v[2];
+        string log = "[vtm" + to_string(machineNo) + "]: " + v[4] + "," + v[3] + "TL" + "," + v[2];
         
         pthread_mutex_lock(&writeLock);
         fileOut << log << endl;
